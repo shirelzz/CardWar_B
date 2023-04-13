5e0b3aadf17c8f3b0912de0ee1d8ae58fc2c312f
shirelzz <shirelzz.2016@gmail.com>
2023-04-13 23:14:50 +0300

    add implementation

--- sources/game.cpp
+++ sources/game.cpp
@@ -4,10 +4,21 @@
 namespace ariel{}
 
 #include <vector>
+#include <string>
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <random>
 
+using std::vector;
+using std::string;
 
-Game::Game(Player pla1, Player pla2 ): player1(pla1), player2(pla2){}
+Game::Game(Player pla1, Player pla2 ): player1(pla1), player2(pla2) {
+    createDeck();
+    shuffleDeck(this->deck);
+    dealCards(this->deck);
+}
 
 // Game::~Game(){}
 
@@ -19,25 +30,52 @@ void Game::printStats(){}
 
 void Game::playTurn(){
 
+    if (player1.equals(player2))
+    {
+        throw std::runtime_error("Player can't play against himself");
+    }
+
+
+    
+
 }
 
 void Game::printLastTurn(){}
 
 void Game::playAll(){}
 
-void Game::shuffleDeck(){
+vector<Card> Game::getDeck(){
+    return this->deck;
+}
+
+void Game::createDeck(){
+    for (int i = 1; i < 13; i++)
+    {
+        deck.push_back(Card(i, "Clubs"));
+        deck.push_back(Card(i, "Diamonds"));
+        deck.push_back(Card(i, "Hearts"));
+        deck.push_back(Card(i, "Spades"));
+    }   
     
 }
 
-void Game::dealCards(std::vector<Card> deck_of_cards){
-    int half = deck_of_cards.size() / 2;
-    for (int i = 0; i < half; i++)
-    {
-        this->player1.takeCard(deck_of_cards[i]);
-        this->player2.takeCard(deck_of_cards[i+1]);
+void Game::shuffleDeck(vector<Card> cards){
+
+    int n = cards.size();
+
+    for (int i = n-1 ; i>=0; i--){
+        int rnd_idx = rand() % (i+1);
+        swap(cards[i], cards[rnd_idx]);
 
     }
-    
+
+}
+
+
+void Game::dealCards(vector<Card> cards){
+    int mid = cards.size() / 2;
+        this->player1.dealCards_pl(cards, 0 , mid);
+        this->player2.dealCards_pl(cards, mid , cards.size());
 }
 
 
